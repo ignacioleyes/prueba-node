1f20aca053d69ac73ad2067e52fa2077147bfb2a
IgnacioLeyes <juanignacioleyes@gmail.com>
2021-03-23 15:30:52 -0300

    Creando mi primer commit

---
+++ calculadora/index.js
@@ -0,0 +1,12 @@
+const sumar = require("./sumar");
+const restar = require("./restar");
+const multiplicar = require("./multiplicar");
+const dividir = require("./dividir");
+
+console.log(sumar(1,2));
+console.log(restar(2,1));
+console.log(multiplicar(0,2));
+console.log(dividir(5,0));
+
+
+
